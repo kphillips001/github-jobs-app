774d0e431a759a19e280bb52017a6cb37d9f7749
Kevin <knphillips01@gmail.com>
2020-09-16 21:05:09 -0500

    setup pagination

--- src/useFetchJobs.js
+++ src/useFetchJobs.js
@@ -4,7 +4,8 @@ import axios from 'axios';
 const ACTIONS = {
   MAKE_REQUEST: 'make-request',
   GET_DATA: 'get-data',
-  ERROR: 'error'
+  ERROR: 'error',
+  UPDATE_HAS_NEXT_PAGE: 'update-has-next-page'
 }
 
 const BASE_URL = 'https://cors-anywhere.herokuapp.com/https://jobs.github.com/positions.json'
@@ -26,6 +27,11 @@ function reducer(state, action) {
         loading: false, 
         error: action.payload.error,
       jobs: []}
+    case ACTIONS.UPDATE_HAS_NEXT_PAGE:
+      return {
+        ...state,
+        hasNextPage: action.payload.hasNextPage
+      }
     default: 
       return state;
   }
@@ -35,24 +41,34 @@ export default function useFetchJobs(params, page) {
   const [state, dispatch] = useReducer(reducer, { jobs: [], loading: true} )
 
   useEffect(() => {
-    const cancelToken = axios.CancelToken.source()
+    const cancelToken1 = axios.CancelToken.source()
     dispatch({ type: ACTIONS.MAKE_REQUEST })
     axios.get(BASE_URL, {
-      cancelToken: cancelToken.token, 
+      cancelToken: cancelToken1.token,
       params: { markdown: true, page: page, ...params }
     }).then(res => {
-       dispatch({ type: ACTIONS.GET_DATA, payload: { jobs: res.data} }) 
+      dispatch({ type: ACTIONS.GET_DATA, payload: { jobs: res.data } }) 
     }).catch(e => {
       if (axios.isCancel(e)) return
-      dispatch({ type: ACTIONS.ERROR, payload: { error: e} }) 
+      dispatch({ type: ACTIONS.ERROR, payload: { error: e } }) 
+    })
+
+    const cancelToken2 = axios.CancelToken.source()
+    axios.get(BASE_URL, {
+      cancelToken: cancelToken2.token,
+      params: { markdown: true, page: page + 1, ...params }
+    }).then(res => {
+      dispatch({ type: ACTIONS.UPDATE_HAS_NEXT_PAGE, payload: { hasNextPage: res.data.length !== 0 } }) 
+    }).catch(e => {
+      if (axios.isCancel(e)) return
+      dispatch({ type: ACTIONS.ERROR, payload: { error: e } }) 
     })
 
     return() => {
-      cancelToken.cancel()
+      cancelToken1.cancel()
+      cancelToken2.cancel()
     }
   }, [params, page])
-
-  
 
   return state
 }

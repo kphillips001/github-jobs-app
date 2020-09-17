155eafca519fb0f534053a07d2631070f01f00db
Kevin <knphillips01@gmail.com>
2020-09-16 19:26:33 -0500

    created useFetchJobs hook

---
+++ src/App.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import useFetchJobs from './useFetchJobs';
+import { Container } from 'react-bootstrap';
+ 
+function App() {
+  const { jobs, loading, error } = useFetchJobs()
+
+  return (
+    <Container>
+      {loading && <h1>Loading...</h1>}
+      {loading && <h1>Error. Try Refreshing.</h1>}
+      {<h1>{jobs.length}</h1>}
+    </Container>
+  );
+}
+
+export default App;
